f5e230aceab6e449ac45ce93c9f67580ef381cb7
Bell217 <izanagy02@gmail.com>
2026-06-06 00:48:03 +0200

    Add files via upload

---
+++ HomeStepDefs.java
@@ -0,0 +1,27 @@
+package hotel;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Objects;
+
+
+public class HomeStepDefs {
+    WebDriver driver;
+
+    @Given("the user opens the homepage")
+    public void openHome() {
+        driver = new ChromeDriver();
+        driver.get("https://automationintesting.online/");
+    }
+
+    @Then("the page title contains {string}")
+    public void checkTitle(String text) {
+        System.out.println("TITLE: " + driver.getTitle());
+        Assert.assertTrue(driver.getTitle().contains(text));
+        driver.quit();
+    }
+}
